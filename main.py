8390c7d74476b96c8584af6e006af4a252f012c3
Leandro Outi Baroni <64474545+LiuDuty@users.noreply.github.com>
2025-11-03 15:27:16 -0300

    fix:goback

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ API_KEY = "03038b49c41b4bbdb1ce54888b54d223.cOjmjTibnl3uqERW"
 API_URL = "https://api.z.ai/api/paas/v4/chat/completions"
 DB_FILE = "conversas.db"
 RENDER_URL = "https://chatzai.onrender.com"
-FRONTEND_URL = "https://quiz-azure.vercel.app"
+FRONTEND_URL = "https://chat-zai-frontend.vercel.app"
 
 SYSTEM_PROMPT = (
  """🎯 **Oi! Sou o QUIZ Azure** — seu assistente dedicado exclusivamente ao **Microsoft Azure Fundamentals (AZ-900)**!
